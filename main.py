c139d5b291952afb758beb97f4e6f2d9b1bfdb22
Szymon Kozlowski <szymon.p.kozlowski@gmail.com>
2025-01-31 14:18:39 +0000

    Further changes to backpropagation; Still not complete

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import numpy as np
 
 testPath = "mnist_test.csv"
 with open(testPath, "r") as f:
-    tests = np.array([[int(n) if i == 0 else int(n) / 255 for i, n in enumerate(line.strip().split(","))] for line in f.readlines()])
+    tests = np.array([[int(n) if i == 0 else int(n) / 255 for i, n in enumerate(line.strip().split(","))] for line in f.readlines()[:8000]])
 
 training = []
 for i in range(2):
     trainPath = f"mnist_train{i}.csv"
     with open(trainPath, "r") as f:
-        training.extend([[int(n) if i == 0 else int(n) / 255 for i, n in enumerate(line.strip().split(","))] for line in f.readlines()])
+        training.extend([[int(n) if i == 0 else int(n) / 255 for i, n in enumerate(line.strip().split(","))] for line in f.readlines()[:4000]])
 training = np.array(training)
 
 
 batchSize = 10
 tests = tests.reshape(-1, batchSize, 785)
 training = training.reshape(-1, batchSize, 785)
-print(training.shape)
 bot = Bot.AI()
+
+bot.test(tests)
 
 bot.train(training)
 bot.test(tests)
+#print(bot.layers[0])
--- mnistBot.py
+++ mnistBot.py
@@ -6,16 +6,24 @@ class AI:
 
     def setup(self):  # Layer(input amount, output amount)
         self.layers = []
-        #self.layers.append(ReLU(784, 16, first=True))
-        #self.layers.append(ReLU(16, 16))
-        #self.layers.append(SoftMax(16, 10))
-        self.layers.append(SoftMax(784, 10))
+        self.layers.append(ReLU(784, 16, first=True))
+        self.layers.append(ReLU(16, 16))
+        self.layers.append(SoftMax(16, 10))
+        #self.layers.append(SoftMax(784, 10))
+
+
+    def MSEDer(self, labels, results):
+        return labels - results
+
+    def crossEntropyDer(self, labels, results):
+        pass
 
     def train(self, inputs):  # To confirm if works
         for batch in inputs:
             labels, results = self.run(batch, der=True)
-            # cost = 1/2 * (labels - results) ** 2
-            der = labels - results
+            der = self.MSEDer(labels, results)
+            #der = self.crossEntropyDer(labels, results)
+            #print(der)
             #print(f"Labels: {labels.shape}\nResults: {results.shape}")
             #print(der)
             for layer in self.layers[::-1]:
@@ -33,7 +41,7 @@ class AI:
             for i in range(batchSize):
                 if labels[i] == results[i]:
                     correct += 1
-            print(f"{correct/total * 100:.5f}%")
+        print(f"{correct/total * 100:.5f}%")
 
 
 
@@ -41,6 +49,7 @@ class AI:
     def run(self, batch, der=False):
         labels = np.array([[0 if i != int(line[0]) else 1 for i in range(10)] for line in batch])
         results = np.array([line[1:] for line in batch])
+        #print(np.average(results), np.sum(results))
         for layer in self.layers:
             results = layer.forward(results, der)
         return labels, results
@@ -49,21 +58,25 @@ class Layer:
     def __init__(self, inputs, ouputs, first=False):  # The amount of inputs this layer will take in, and the amount of outputs this layer will return
         self.genNodes(inputs, ouputs)
         self.rate = 0.05  # To further modify so its not static
-        self.first=False
+        self.first=first
 
     def forward(self, input, der=False):
+        #print(input.shape, self.w.shape, self.b.shape)
         computation = (input @ self.w) + self.b
         if der:
             self.input = input
+        #print(computation)
         return self.activation(computation, der)
 
-    def backward(self, der):
-        biasDer = self.activationDer(der)
-        self.b -= self.rate * np.average(biasDer, axis=0)
+    def backward(self, der):  # Input shapes are 100% correct;
+        biasDer = self.activationDer(der)  # Calculates the bias Derivative by multiplying derivative from previous layer/cost function
+                                           # With the derivative of the activation function
+        self.b -= self.rate * np.mean(biasDer, axis=0)  #np.sum(biasDer, axis=0)  # the rate is defined earlier in my code as self.rate = 0.000001
         weightsDer = self.input.T @ biasDer
-        self.w -= self.rate * np.average(weightsDer, axis=0)
+        self.w -= self.rate * np.mean(weightsDer, axis=0) #np.sum(weightsDer, axis=0)
 
-        if not self.first:
+        if not self.first:  # This just checks if its the first layer in the forward pass
+            # Because if it is, it does not need to return any derivative
             der = biasDer @ self.w.T
             return der
 
@@ -98,7 +111,7 @@ class SoftMax(Layer):
         return act
 
     def activationDer(self, der):
-        return der  # Crazy confusing but this is actually correct
+        return -der  # Crazy confusing but this is actually correct
 
     def genNodes(self, inputs, outputs):  # Xavier Initialisation
         self.w = np.random.normal(0, np.sqrt(1 / inputs), (inputs, outputs))
